2cb6b8cf0ddbb6d908b5a09becce80da93ba020f
steffenkj <steffenkja@gmail.com>
2020-01-26 21:47:27 +0100

    Fixed nan bug in stim value setting, made code fit generic input dataframe

--- main.py
+++ main.py
@@ -3,9 +3,13 @@ Implementation of AIRS2 (Artificial Immune Recognition System V2) applied to the
 @author : Azzoug Aghiles
 """
 
-import math
 import random
 import time
+from sklearn.model_selection import train_test_split
+from sklearn import preprocessing
+import pandas as pd
+from scipy.spatial.distance import pdist
+import numpy as np
 
 
 class AIRS:
@@ -21,10 +25,24 @@ class AIRS:
         k (int) : The number of memory cells to use for classification
         test_size (float) : The percentage of global data to take as test data
 
+    This implementation uses negative selection and clonal selection to imitate
+    B-Cells in the human immune system.
+    
+    Optimizations:
+        - Calculation of distance to all neighbors must be reordered into a 
+        more effective knn algorithm, horribly slow at the moment.
+    Glossary
+    ---------------------------------------------------------------------------
+    
+    MC: Memory Cells
+    
     """
 
-    def __init__(self, hyper_clonal_rate, clonal_rate, class_number, mc_init_rate,
-                 total_num_resources, affinity_threshold_scalar, k, test_size):
+    def __init__(self, hyper_clonal_rate, clonal_rate, class_number,
+                 mc_init_rate, total_num_resources, affinity_threshold_scalar,
+                 k, test_size, create_test_train_set=True, train_set=None,
+                 test_set=None, input_data_file=None, data=None):
+        
         self.HYPER_CLONAL_RATE = hyper_clonal_rate
         self.CLONAL_RATE = clonal_rate
         self.AFFINITY_THRESHOLD = 0
@@ -36,6 +54,15 @@ class AIRS:
         self.K = k
         self.MC = None
         self.AB = None
+        self.input_data_file = input_data_file
+        self.data = data
+
+        if create_test_train_set:
+            self.train_set, self.test_set = self.train_test_split()
+        else:
+            self.train_set = train_set
+            self.test_set = test_set
+        
 
     @staticmethod
     def affinity(vector1, vector2):
@@ -44,39 +71,104 @@ class AIRS:
         :param vector2: Second features vector
         :return: The affinity between the two vectors [0-1]
         """
+        
+        #euclidian_distance = 0
+        #d = 0
+        
+        dist = np.linalg.norm(vector1 - vector2)
+        
+        #for i, j in zip(vector1, vector2):
+        #    d += (i - j) ** 2
+        #euclidian_distance = math.sqrt(d)
+        #return euclidian_distance / (1 + euclidian_distance)
+        
+        return dist/(1 + dist)
+    
+    
+    def train_test_split(self, open_file=True):
+        
+        df = self.data #pd.read_csv(self.input_data_file)
 
-        euclidian_distance = 0
-        d = 0
-        for i, j in zip(vector1, vector2):
-            d += (i - j) ** 2
-        euclidian_distance = math.sqrt(d)
-        return euclidian_distance / (1 + euclidian_distance)
+        y = df.iloc[:, -1]
+        X = df.iloc[:, :-1]
 
-    def train_test_split(self):
-        with open("iris.data", "r") as data:
-            content = data.readlines()
-            ret = [([float(x.split(",")[i]) for i in range(4)], mapping[x.split(",")[4][:-1]]) for x in content]
-            random.shuffle(ret)
-        return ret[:int((1 - self.TEST_SIZE) * len(ret))], ret[int((1 - self.TEST_SIZE) * len(ret)):]
+        X_train, X_test, y_train, y_test = train_test_split(X, y,
+                                                            test_size=0.33,
+                                                            random_state=42)
 
-    def calculate_affinity_threshold(self):
-        train_set, test_set = self.train_test_split()
-        affinity_threshold = 0
-        for i in range(len(train_set)):
-            for j in range(i + 1, len(train_set)):
-                affinity_threshold += self.affinity(train_set[i][0], train_set[j][0])
+        train_set = pd.concat([X_train, y_train], axis=1)
+        test_set = pd.concat([X_test, y_test], axis=1)
 
-        self.AFFINITY_THRESHOLD = affinity_threshold / (len(train_set) * (len(train_set) - 1) / 2)
+        return train_set[:20000], test_set[:6660]
 
-    def init_MC(self, train_set, MC):
+       # if open_file:
+       #     with open(self.input_data_file, "r") as data:
+       #         content = data.readlines()
+       #         ret = [([float(x.split(",")[i]) for i in range(4)],
+       #                  mapping[x.split(",")[4][:-1]]) for x in content]
+       #         random.shuffle(ret)
+       #     return ret[:int((1 - self.TEST_SIZE) * len(ret))], ret[int((1 - self.TEST_SIZE) * len(ret)):]
+        
+       # else:
+            
+       #     y = self.data[:, -1]
+       #     X = self.data[:, :-1]
+
+       #     X_train, X_test, y_train, y_test = train_test_split(X, y,
+       #                                                         test_size=0.33,
+       #                                                         random_state=42)
+            
+       #     train_set = pd.concat(X_train, y_train, axis=1)
+       #     test_set = pd.concat(X_test, y_test, axis=1)
+
+       #     return train_set, test_set
+
+
+
+    def calculate_affinity_threshold(self, use_brute_force=False):
+        """
+        Calculates euclidian distance between the datapoints.
+        Since we can have a large nr. of features and a very large nr. of 
+        rows, this is very computationally expensive and should be optimized.
+        
+        Perhaps use Cython.
+        """
+        print("Now calculating affinity threshold..")
+        if use_brute_force:
+            affinity_threshold = 0
+            print('Calculating affinity threshold..')
+            for i in range(len(self.train_set)):
+                for j in range(i + 1, len(self.train_set)):
+                    affinity_threshold += self.affinity(self.train_set.iloc[i, :-1], self.train_set.iloc[j, :-1])
+                    
+            self.AFFINITY_THRESHOLD = affinity_threshold / (len(self.train_set) * (len(self.train_set) - 1) / 2)
+        else:
+            
+            # Affinity threshold should be normalized
+            x = self.train_set.iloc[:, :-1].values  # returns a numpy array
+            min_max_scaler = preprocessing.MinMaxScaler()
+            x_scaled = min_max_scaler.fit_transform(x)
+            df = pd.DataFrame(x_scaled)
+ 
+            self.AFFINITY_THRESHOLD = np.mean(pdist(df.values))
+            
+#            self.AFFINITY_THRESHOLD = np.mean(pdist(self.train_set.iloc[:, :-1].values))
+            
+            print(f"Affinity threshold found as {self.AFFINITY_THRESHOLD}!")
+
+
+    def init_MC(self, MC):
         """ Init the memory set pool
         :param train_set: the training set
         :param MC: The memory set pool
         """
-        for _ in range(int(len(train_set) * self.MC_INIT_RATE)):
-            seed_cell = random.choice(train_set)
-
-            MC[seed_cell[1]].append(Cell(vector=seed_cell[0], _class=seed_cell[1]))
+        print("Initiating memory set pool..")
+        for _ in range(int(len(self.train_set) * self.MC_INIT_RATE)):
+            seed_cell = random.choice(self.train_set.values)
+          #  print("seed", seed_cell)
+          #  print("MC", MC)
+          #  print("CELL", Cell(vector=seed_cell[0], _class=seed_cell[-1]))
+            MC[int(seed_cell[-1])].append(Cell(vector=seed_cell[0], _class=seed_cell[-1]))
 
     def argminARB(self, AB, _class):
         """Get the ARB with the minimum amount of resources
@@ -103,28 +195,34 @@ class AIRS:
         """
         maxStim = 0.0
         ab = None
+#        print("AB", AB)
+#        print("AB class", AB[_class])
+        
+#        print("len of AB class", len(AB[_class]))
+        
         for i in range(len(AB[_class])):
             if AB[_class][i].stimulation >= maxStim:
                 maxStim = AB[_class][i].stimulation
                 ab = AB[_class][i]
+        
         c = Cell(vector=ab.vector, _class=ab._class)
         c.stimulation = ab.stimulation
         return c
 
     def train(self):
-        """Training method"""
+        """Train AIRS on the training dataset"""
         start = time.time()
-        train_set, test_set = self.train_test_split()
 
+        # Calculate the affinity threshold of the memory cells
         self.calculate_affinity_threshold()
         MC = {_class: [] for _class in range(self.CLASS_NUMBER)}
         AB = {_class: [] for _class in range(self.CLASS_NUMBER)}
 
         # MC Initialisation
-        self.init_MC(train_set, MC)
-
-        for antigene, _class in train_set:
-
+        self.init_MC(MC)
+#        print(self.train_set.iloc[:, :-1].values)
+        for row in self.train_set.values:
+            antigene, _class = row[:-1], row[-1]
             # MC Identification
             mc_match = None
             if len(MC[_class]) == 0:
@@ -158,7 +256,10 @@ class AIRS:
 
                 MIN_STIM = 1.0
                 MAX_STIM = 0.0
-
+                
+                # BUG! MIN_STEM and MAX_STEM can appear as same value at end of loop.
+                # Error appears if stim is 0<stim<1.
+                # This must be an unexpected value, indicating a possible error.
                 for c in AB.keys():
                     for ab in AB.get(c):
                         stim = ab.stimulate(antigene)
@@ -166,6 +267,11 @@ class AIRS:
                             MIN_STIM = stim
                         if stim > MAX_STIM:
                             MAX_STIM = stim
+
+                if MIN_STIM == MAX_STIM:
+                    print(f"OBS! 0<stim({stim})<1, keeping MIN_STEM and MAX_STEM at default values")
+                    MIN_STIM = 1.0
+                    MAX_STIM = 0.0                    
 
                 for c in AB.keys():
                     for ab in AB.get(c):
@@ -183,7 +289,7 @@ class AIRS:
                     else:
                         AB[_class][abRemoveIndex].resources -= numResRemove
                         resAlloc -= numResRemove
-
+               # print(f"avgStim is {avgStim}")
                 if (avgStim > self.AFFINITY_THRESHOLD) or (iterations >= MAX_ITER):
                     break
 
@@ -201,13 +307,18 @@ class AIRS:
         self.AB = AB
 
         n_correct = 0
-        for ag, _class in test_set:
+        
+        for row in self.test_set.values:
+            ag = row[:-1]
+            _class = row[-1]
             if self.classify(ag) == reverseMapping[_class]:
                 n_correct += 1
+        #for ag, _class in (self.test_set.iloc[:, :-1], self.test_set.iloc[:, -1]):
+
 
         print("Execution time : {:2.4f} seconds".format(time.time() - start))
-        print("Accuracy : {:2.2f} %".format(n_correct * 100 / len(test_set)))
-        return n_correct / len(test_set)
+        print("Accuracy : {:2.2f} %".format(n_correct * 100 / len(self.test_set)))
+        return n_correct / len(self.test_set)
 
     def classify(self, antigene):
         if (self.MC is None) or (self.AB is None):
@@ -259,11 +370,28 @@ class ARB:
                                                                               self.stimulation, self.resources)
 
     def stimulate(self, pattern):
+        """
+        Stimulation of the ARB is inversely proportional with the affinity of
+        the cell. Affinity ranges from [0;1] so stimulation ranges from 
+        [1;0] due to start value of stim of 1.
+        
+        Parameters
+        ----------
+        pattern : TYPE
+            DESCRIPTION.
+
+        Returns
+        -------
+        self.stimulation: float
+            Stimulation value of the cell.
+
+        """
+        
         self.stimulation = 1 - AIRS.affinity(vector1=pattern, vector2=self.vector)
         return self.stimulation
 
     def mutate(self):
-        _range = 1 - self.stimulation
+        # _range = 1 - self.stimulation
         mutated = False
         new_vector = []
 
@@ -324,22 +452,36 @@ class Cell:
 
 
 if __name__ == '__main__':
-    # 6.3,2.3,4.4,1.3,Iris-versicolor
-    # 5.1, 2.5, 3.0, 1.1, Iris - versicolor
-    # 6.9,3.1,5.1,2.3,Iris-virginica
+
+    # =========================================================================
+    # USE-CASE EXAMPLE ON IRIS DATASET
+    # =========================================================================
 
     ARRAY_SIZE = 4  # Features number
     MAX_ITER = 5  # Max iterations to stop training on a given antigene
 
     # Mapping classes to integers
-
     mapping = {"Iris-setosa": 0, "Iris-versicolor": 1, "Iris-virginica": 2}
-    reverseMapping = {0: "Iris-setosa", 1: "Iris-versicolor", 2: "Iris-virginica"}
+    reverseMapping = {0: "Iris-setosa", 1: "Iris-versicolor",
+                      2: "Iris-virginica"}
 
     # Mutation rate for ARBs
     MUTATION_RATE = 0.2
-
-    airs = AIRS(hyper_clonal_rate=20, clonal_rate=0.8, class_number=3, mc_init_rate=0.4,
-                total_num_resources=10, affinity_threshold_scalar=0.8, k=6, test_size=0.4)
+    
+    data = pd.read_csv('data/creditcard.csv')
+    
+    # Very low nr of fraud cases, upsample cases.
+    data = data.append([data[data.loc[:, 'Class']==1]]*100, ignore_index=True)
+    
+    airs = AIRS(hyper_clonal_rate=20,
+                clonal_rate=0.8,
+                class_number=2,
+                mc_init_rate=0.4,
+                total_num_resources=10,
+                affinity_threshold_scalar=0.8,
+                k=6,
+                test_size=0.4,
+                data=data)
+                #input_data_file='data/creditcard.csv')
 
     airs.train()
